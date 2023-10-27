d3e06a171ca7ff2fcdf44ae7e1a16fe7a1fb5876
Haider Kamran <haiderkamrann@gmail.com>
2023-10-27 17:19:00 +0500

    Using Babel and WebPack Manually

--- ManualReact-app/app.js
+++ ManualReact-app/app.js
@@ -1,4 +1,4 @@
 import React from "react"
 import  ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>First App</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>First App in React</h1>, document.getElementById('root'))
